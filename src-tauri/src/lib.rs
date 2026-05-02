62a1dc480f8ad31c3c77f72875124f79eb2674dd
Sóstenes Freitas <sostenesfreitas@gmail.com>
2026-05-02 08:58:19 -0300

    fix: register tauri-plugin-opener in Tauri builder for external link support

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,6 +10,7 @@ fn get_api_port(state: tauri::State<'_, ApiPort>) -> u16 {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_shell::init())
         .manage(ApiPort(Mutex::new(7842)))
         .setup(|app| {
